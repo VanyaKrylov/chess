479e3e1899adcf626c95f017a54d8a0b31c4293f
user <user@debian>
2016-03-14 03:50:18 +0300

    doxygen report

---
+++ report/Doxygen/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['loadgame',['LoadGame',['../classAPI.html#a7afde1ccf05be314d6a9c6194a556c7a',1,'API']]]
+];
---
+++ report/Doxygen/html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['movefigure',['MoveFigure',['../classAPI.html#a4e06a33fd2468704bcc53bb8e2e769e0',1,'API']]]
+];

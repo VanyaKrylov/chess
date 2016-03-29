d1e8b9935f3c2fef384214e1d9e661976bd018ac
user <user@debian>
2016-03-30 01:02:20 +0300

    Doxy docs deleted

--- report/Doxygen/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['movefigure',['MoveFigure',['../classAPI.html#a4e06a33fd2468704bcc53bb8e2e769e0',1,'API']]]
-];
--- report/Doxygen/html/search/functions_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['savegame',['SaveGame',['../classAPI.html#aa9c60516d733f42bbb67bf4efe4a67b8',1,'API']]],
-  ['setgamesettings',['SetGameSettings',['../classAPI.html#a6596a30f24a77cd1d2821dae55e87702',1,'API']]],
-  ['startgame',['StartGame',['../classAPI.html#a5398b46cb85a8f028fe430110066dd61',1,'API']]],
-  ['startnewgame',['StartNewGame',['../classAPI.html#a7f7e264073e3d6162a0992fe52ef8e82',1,'API']]]
-];
